1b2c53bc0602f09a4e4a392f519aea6d2cdf27fa
Ken Turnbull <Turnbullken@gmail.com>
2018-07-01 13:45:28 -0700

    New files and package.json

---
+++ node-experiment/test.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var https = require('https');
+
+fs.writeFile(__dirname + '/index.html','<h1>HTML is great</h1>',function(error){
+    if(error){
+        return console.log(error);
+    } else{
+        console.log('Congrats');
+    }
+});
+
+// var myPhotoUrl = 'https://github.com/LearnWebCode/welcome-to-git/blob/master/images/dog.jpg?raw=true';
+// https://github.com/LearnWebCode/welcome-to-git/blob/master/images/dog.jpg?raw=true
+var myPhotoUrl = 'https://raw.githubusercontent.com/LearnWebCode/welcome-to-git/master/images/dog.jpg';
+https.get(myPhotoUrl,function(response) {
+    response.pipe(fs.createWriteStream(__dirname + '/mydog.jpg'));
+});
